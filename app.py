741749375d85bccbe9278117c4d1c39f13c8092b
Klever <kleverbaliza9@gmail.com>
2026-03-06 16:52:47 -0300

    Correcao de porta para deploy no Render

--- app.py
+++ app.py
@@ -575,4 +575,5 @@ def avaliar(ag_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
